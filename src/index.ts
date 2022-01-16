3507c1c59cb6e0333fb312b0a0b7523ec4285ab0
Allusive <auraud.zarafshar@gmail.com>
2022-01-16 12:44:56 -0800

    Done

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,30 @@ import dotenv from "dotenv-safe";
 import qs from "qs";
 import express from "express";
 import axios from "axios";
-import fetch from "node-fetch";
+import passport from "passport";
+import { Strategy as SpotifyStrategy } from "passport-spotify";
+
 const app = express();
 dotenv.config();
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: clientId,
+      clientSecret: clientSecret,
+      callbackURL: "http://localhost:3000/callback",
+      scope: ["user-read-email", "user-read-private"],
+    },
+    (accessToken, refreshToken, expires_in, profile, done) => {
+      accessToken = accessToken;
+      refreshToken = refreshToken;
+      expires_in = expires_in;
+      console.log(profile);
+      done(null, profile);
+    }
+  )
+);
 
 var spotifyApi = new SpotifyWebApi({
   clientId: clientId,
@@ -16,47 +35,16 @@ var spotifyApi = new SpotifyWebApi({
 });
 
 app.get("/", (req, res) => {
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      qs.stringify({
-        response_type: "code",
-        client_id: clientId,
-        scope: "user-read-private user-read-email",
-        redirect_uri: "http://localhost:3000/callback",
-        state: "some-state-of-my-choice",
-      })
-  );
+  res.send("Hello World!");
+}
+
+app.get("/auth", passport.authenticate("spotify"));
+
+app.get("/callback",passport., (req, res) => {
+
+  res.send("You are now logged in!");
 });
 
-app.get("/callback", (req, res) => {
-  const code = req.query.code || null;
-  const authEndpoint = "https://accounts.spotify.com/api/token";
-  const authHeaders = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Authorization:
-      "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64"),
-  };
-  const authBody = {
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: "http://localhost:3000/callback",
-  };
-  const authRequest = {
-    method: "POST",
-    headers: authHeaders,
-    body: qs.stringify(authBody),
-  };
-  fetch(authEndpoint, authRequest)
-    .then((response) => response.json())
-    .then((json: any) => {
-      spotifyApi.setAccessToken(json.access_token);
-      spotifyApi.setRefreshToken(json.refresh_token);
-      spotifyApi.getMe().then((data) => {
-        console.log(data.body);
-      });
-      res.send(json);
-    });
-});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");

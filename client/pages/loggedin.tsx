555bb2ffdf507884e5dfe51edf0020b139e01f3c
Allusive <auraud.zarafshar@gmail.com>
2022-01-16 22:46:43 -0800

    Done with basic player

--- client/pages/loggedin.tsx
+++ client/pages/loggedin.tsx
@@ -1,24 +1,37 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
 const loggedin = () => {
-  // Make an API call to get the user's data
-  // If the user is logged in, display their name and avatar
-  // If the user is not logged in, display a login button
   const [playing, setPlaying] = useState<any>([]);
   const [image, setImage] = useState<any>("");
+  const [artist, setArtist] = useState<any>([{}]);
+  const [pos, setPos] = useState<any>(0);
+
+  let loggedIn = true;
+
   useEffect(() => {
     axios
       .get("http://localhost:4000/checklogin", { withCredentials: true })
       .then((res) => {
         if (res.status === 200) {
+          loggedIn = true;
           axios
             .get("http://localhost:4000/playing", { withCredentials: true })
             .then((res) => {
               console.log(res.data);
               console.log(res.data.album.images[0].url);
               setImage(res.data.album.images[0].url);
+              console.log(res.data.artists[0].name);
+              setArtist(res.data.artists);
               setPlaying(res.data);
+              console.log(res.data.duration_ms);
+            });
+          axios
+            .get(`http://localhost:4000/pos`, {
+              withCredentials: true,
+            })
+            .then((res) => {
+              console.log(res);
+              setPos(res.data);
             });
         }
       });
@@ -26,9 +39,54 @@ const loggedin = () => {
 
   return (
     <div>
-      <h1>Logged In</h1>
       <h2>{playing.name}</h2>
-      <img src={image} height={100} width={100} />
+      {artist.map((artist: any) => (
+        <h3 key={artist.id}>{artist.name}</h3>
+      ))}
+      <img src={image} key={artist.id} height={100} width={100} />
+      <input
+        type="range"
+        min="0"
+        max={playing.duration_ms}
+        value={pos}
+        onTimeUpdate={(e) => {
+          setPos(pos);
+        }}
+        onChange={(e) => {
+          console.log(e.target.value);
+          console.log(loggedIn);
+          setPos(e.target.value);
+          axios({
+            method: "put",
+            url: `http://localhost:4000/playing?pos=${e.target.value}`,
+            withCredentials: true,
+          });
+        }}
+      />
+
+      <button
+        onClick={() => {
+          axios({
+            method: "post",
+            url: `http://localhost:4000/play`,
+            withCredentials: true,
+          });
+        }}
+      >
+        Play
+      </button>
+
+      <button
+        onClick={() => {
+          axios({
+            method: "post",
+            url: `http://localhost:4000/pause`,
+            withCredentials: true,
+          });
+        }}
+      >
+        Pause
+      </button>
     </div>
   );
 };
